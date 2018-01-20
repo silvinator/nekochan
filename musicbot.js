e32693bda0e103f46f1222f544b7f164a1ea4b06
silvinator <rodewald.mr@gmail.com>
2018-01-20 18:15:29 +0100

    Update musicbot.js

--- musicbot.js
+++ musicbot.js
@@ -423,7 +423,7 @@ exports.run = function(server_name, text_channel_name, voice_channel_name, alias
 		console.log("Connected!");
 	});
 
-	bot.login(token);
+	bot.login(NDA0Mjc1MDc2NjI0NTQ3ODYw.DUUJOA.kj2e_dt99tIApwQMqTC9dnKhiUw);
 }
 
 exports.setYoutubeKey = function(key) {
